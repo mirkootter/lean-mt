a01938ced40494471f99f28c4093aac63af82318
Jan-Mirko Otter <janmirko.otter@googlemail.com>
2022-09-26 17:19:34 +0200

    added docgen4 facet

--- lakefile.lean
+++ lakefile.lean
@@ -9,3 +9,6 @@ package mt {
 lean_lib Mt {
   -- add library configuration options here
 }
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
