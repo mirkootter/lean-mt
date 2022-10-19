1b8bd74792a701b192aee81b68df60d7f8a86bcf
Jan-Mirko Otter <janmirko.otter@googlemail.com>
2022-10-19 15:35:10 +0000

    more doc comments

--- Mt/Thread/Basic.lean
+++ Mt/Thread/Basic.lean
@@ -3,6 +3,9 @@ import Mt.Utils
 
 namespace Mt
 
+/-- A single thread of the system. It consists of a task to
+  execute and a blocking predicate. The system only
+  iterates a thread if the blocking predicate holds -/
 structure Thread (spec : Spec) where
   T : Type
   block_until : spec.State -> Bool
@@ -36,6 +39,13 @@ def iterate : Thread spec -> spec.State -> IterationResult spec
     | TaskM.IterationResult.Running state' block_until task => 
       IterationResult.Running state' { T, block_until, task }
 
+/-- Small wrapper around `TaskM.valid`. A thread is considered valid
+  if its underlying task is valid and ensures that it drops its
+  reservation in the end.
+  
+  To prove `Thread.valid`, you should unfold `Thread.valid`, for example
+  using `rw [Thread.valid]` and continue applying the validation theoreoms
+  for `TaskM.valid`, like `TaskM.valid_bind` -/
 def valid (thread : Thread spec) : Prop :=
   thread.task.valid IsReservation.empty
     thread.block_until
