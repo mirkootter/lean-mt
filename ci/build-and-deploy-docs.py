cab3565cc1b9752d684f2b513bd0553ef8a0fe2a
Jan-Mirko Otter <janmirko.otter@googlemail.com>
2022-10-25 07:17:07 +0000

    started with CI

---
+++ ci/build-and-deploy-docs.py
@@ -0,0 +1,33 @@
+from os import system, environ
+import pathlib
+import shutil
+from subprocess import check_output, run
+
+REPO = 'git@github.com:mirkootter/lean-mt-doc.git'
+
+short_hash = check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('utf-8').strip()
+print("Current repository hash:", short_hash)
+
+commit_message = f"Update to {short_hash}"
+
+check_output(['git', 'clone', '--bare', REPO])
+
+last_commit = check_output(['git', 'log', '-1', '--pretty=format:%s'], cwd='lean-mt-doc.git').decode('utf-8')
+shutil.rmtree('lean-mt-doc.git')
+
+if last_commit == commit_message:
+    print("Already up to date")
+    exit()
+
+print("Build documentation")
+run(['/root/.elan/bin/lake', 'build', '-Kenv=dev', 'Mt:docs'], check = True)
+run(['/root/.elan/bin/lake', 'build', '-Kenv=dev', 'Samples:docs'], check = True)
+
+print("Deploy")
+run(['git', 'init', '-b', 'main'], cwd='build/doc', check = True)
+run(['git', 'config', 'user.name', 'lean-mt-bot'], cwd='build/doc', check = True)
+run(['git', 'config', 'user.email', ''], cwd='build/doc', check = True)
+run(['git', 'add', '-A'], cwd='build/doc', check = True)
+run(['git', 'commit', '-m', commit_message], cwd='build/doc', check = True)
+run(['git', 'remote', 'add', 'origin', REPO], cwd='build/doc', check = True)
+run(['git', 'push', '-f', 'origin', 'main'], cwd='build/doc', check = True)
